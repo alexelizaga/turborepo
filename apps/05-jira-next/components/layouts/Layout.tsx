3c05f71c3dff3cf2cf2cfbe86111431ab53deb82
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-02-22 06:11:22 +0100

    next api

--- apps/05-jira-next/components/layouts/Layout.tsx
+++ apps/05-jira-next/components/layouts/Layout.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import Head from 'next/head';
 
 import { Box } from '@mui/material';
@@ -7,6 +7,7 @@ import { Navbar, Sidebar } from '../ui';
 
 interface Props {
     title?: string;
+    children?: ReactNode;
 }
 
 export const Layout:FC<Props> = ({ title = 'OpenJira', children }) => {
