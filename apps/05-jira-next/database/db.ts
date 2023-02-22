4b67dfd03b6dc8364cc00a0620309e43130cb5c6
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-02-22 06:27:16 +0100

    add next apps

--- apps/05-jira-next/database/db.ts
+++ apps/05-jira-next/database/db.ts
@@ -1,4 +1,4 @@
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 
 /**
  * 0 = disconnected
@@ -7,34 +7,34 @@
  * 3 = disconnecting
  */
 
-// const mongooConnection = {
-//   isConnected: 0
-// }
+const mongooConnection = {
+  isConnected: 0
+}
 
-// export const connect = async() => {
-//   if (mongooConnection.isConnected) {
-//     console.log('We are connected');
-//     return;
-//   }
+export const connect = async() => {
+  if (mongooConnection.isConnected) {
+    console.log('We are connected');
+    return;
+  }
 
-//   if (mongoose.connections.length > 0) {
-//     mongooConnection.isConnected = mongoose.connections[0].readyState;
+  if (mongoose.connections.length > 0) {
+    mongooConnection.isConnected = mongoose.connections[0].readyState;
 
-//     if (mongooConnection.isConnected === 1) {
-//       console.log('Using last connection');
-//       return;
-//     }
+    if (mongooConnection.isConnected === 1) {
+      console.log('Using last connection');
+      return;
+    }
 
-//     await mongoose.disconnect();
-//   }
+    await mongoose.disconnect();
+  }
 
-//   await mongoose.connect('....');
-//   mongooConnection.isConnected = 1;
-//   console.log('MongoDB connected:', '....');
-// }
+  await mongoose.connect('....');
+  mongooConnection.isConnected = 1;
+  console.log('MongoDB connected:', '....');
+}
 
-// export const disconnect = async() => {
-//   if (!mongooConnection.isConnected) return;
-//   await mongoose.disconnect();
-//   console.log('MongoDB disconnected:');
-// }
+export const disconnect = async() => {
+  if (!mongooConnection.isConnected) return;
+  await mongoose.disconnect();
+  console.log('MongoDB disconnected:');
+}
