39e8ed9838ae5c6aeb0cc8b30d55446ced575b80
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-02-24 17:42:55 +0100

    Filling the database

--- apps/05-openjira-next/database/db.ts
+++ apps/05-openjira-next/database/db.ts
@@ -34,6 +34,7 @@ export const connect = async() => {
 }
 
 export const disconnect = async() => {
+  if (process.env.NODE_ENV === 'development') return;
   if (!mongooConnection.isConnected) return;
   await mongoose.disconnect();
   console.log('MongoDB disconnected');
