2c32aaaaa105774d538128c2c91c4efdaab0afd6
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-02-24 04:47:21 +0100

    connect and disconnect to mongo

--- apps/05-openjira-next/database/db.ts
+++ apps/05-openjira-next/database/db.ts
@@ -13,7 +13,7 @@ const mongooConnection = {
 
 export const connect = async() => {
   if (mongooConnection.isConnected) {
-    console.log('We are connected');
+    console.log('We were connected');
     return;
   }
 
@@ -28,13 +28,13 @@ export const connect = async() => {
     await mongoose.disconnect();
   }
 
-  await mongoose.connect('....');
+  await mongoose.connect(process.env.MONGO_URL || '');
   mongooConnection.isConnected = 1;
-  console.log('MongoDB connected:', '....');
+  console.log('MongoDB connected:', process.env.MONGO_URL);
 }
 
 export const disconnect = async() => {
   if (!mongooConnection.isConnected) return;
   await mongoose.disconnect();
-  console.log('MongoDB disconnected:');
+  console.log('MongoDB disconnected');
 }
--- apps/05-openjira-next/pages/api/seed.ts
+++ apps/05-openjira-next/pages/api/seed.ts
@@ -1,19 +1,33 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { db } from '@/database';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   ok: boolean;
   message: string;
-  method?: string;
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+
+  if (process.env.NODE_ENV === 'production') {
+    return res.status(401).json({
+      ok: false,
+      message: 'Does not have access to this service',
+    })
+  }
+
+  await db.connect();
+
+
+
+
+  await db.disconnect();
+
   res.status(200).json({
     ok: true,
-    message: 'Todo correcto',
-    method: req.method
+    message: 'Process done correctly'
   })
 }
