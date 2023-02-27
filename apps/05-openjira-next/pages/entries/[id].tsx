9b4c107dd6e8e74d6640570c4bb91cad337de22c
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-02-27 13:01:47 +0100

    EntryPage

---
+++ apps/05-openjira-next/pages/entries/[id].tsx
@@ -0,0 +1,110 @@
+import { ChangeEvent, useMemo, useState } from 'react';
+import { capitalize, Button, Card, CardActions, CardContent, CardHeader, FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, TextField, IconButton } from "@mui/material";
+import SaveIcon from '@mui/icons-material/SaveOutlined';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+
+import { Layout } from "@/components";
+import { EntryStatus } from "@/interfaces";
+
+const validStatus: EntryStatus[] = ['pending', 'in-progress', 'finished'];
+
+const EntryPage = () => {
+
+  const [inputValue, setInputValue] = useState("");
+  const [status, setStatus] = useState<EntryStatus>("pending");
+  const [touched, setTouched] = useState(false);
+
+  const isNotValid = useMemo(() => !inputValue.length && touched, [inputValue, touched])
+
+  const onInputValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  }
+
+  const onStatusChange = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setStatus(event.target.value as EntryStatus);
+  }
+
+  const onSave = () => {
+
+  }
+
+  return (
+    <Layout title="... ... ...">
+      <Grid
+        container
+        justifyContent="center"
+        sx={{ marginTop: 2 }}
+      >
+        <Grid item xs={ 12 } sm={ 8 } md={ 6 }>
+          <Card>
+            <CardHeader
+              title={`Entrada: ${inputValue}`}
+              subheader={`Created ${'30 min'} ago`}
+            />
+            <CardContent>
+              <TextField
+                sx={{ marginTop: 2, marginBottom: 1 }}
+                fullWidth
+                placeholder="New entry"
+                autoFocus
+                multiline
+                label="New entry"
+                value={inputValue}
+                onBlur={() => setTouched(true)}
+                onChange={onInputValueChange}
+                helperText={ isNotValid && "Enter a value" }
+                error={ isNotValid && touched }
+              />
+
+              <FormControl>
+                <FormLabel>Status:</FormLabel>
+                <RadioGroup
+                  row
+                  value={status}
+                  onChange={onStatusChange}
+                >
+                  {
+                    validStatus.map( option => (
+                      <FormControlLabel
+                        key={option}
+                        value={option}
+                        control={ <Radio />}
+                        label={capitalize(option)}
+                      />
+                    ))
+                  }
+                </RadioGroup>
+              </FormControl>
+
+            </CardContent>
+
+            <CardActions>
+              <Button
+                startIcon={<SaveIcon />}
+                variant={"contained"}
+                fullWidth
+                onClick={ onSave }
+                disabled={!inputValue.length}
+              >
+                Save
+              </Button>
+            </CardActions>
+
+          </Card>
+        </Grid>
+      </Grid>
+
+      <IconButton sx={{
+        position: 'fixed',
+        bottom: 30,
+        right: 30,
+        backgroundColor: 'error.dark'
+      }}>
+        <DeleteIcon />
+      </IconButton>
+    </Layout>
+  )
+}
+
+export default EntryPage;
