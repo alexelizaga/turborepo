fe3c25eccbdfd252f9be73045db3f01ac54a4377
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-02-28 09:02:17 +0100

    new proyect and material ui

---
+++ apps/cookie-master/src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '@/styles/globals.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import type { AppProps } from 'next/app';
+
+import { darkTheme } from '@/themes';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
