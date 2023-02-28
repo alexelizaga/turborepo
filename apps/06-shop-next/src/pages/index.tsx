c4122c7928f7e44ec82a80e304c07d612419ee5a
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-02-28 16:05:39 +0100

    shoplayout

--- apps/06-shop-next/src/pages/index.tsx
+++ apps/06-shop-next/src/pages/index.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from 'next';
+import { Typography } from '@mui/material';
+
+import { ShopLayout } from '@/components';
 
 const HomePage: NextPage = () => {
   return (
-    <>
-      <h1>Shop</h1>
-    </>
+    <ShopLayout title={'Shop - Home'} pageDescription={'Find the best products'}>
+      <Typography variant='h1' component='h1'>Shop</Typography>
+      <Typography variant='h2' sx={{ mb: 1 }}>All the products</Typography>
+    </ShopLayout>
   )
 }
 
