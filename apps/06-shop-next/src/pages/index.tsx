0512a2de56d31eefbc8fdc60e6de2621d0e85135
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-02-28 17:12:38 +0100

    Home page

--- apps/06-shop-next/src/pages/index.tsx
+++ apps/06-shop-next/src/pages/index.tsx
@@ -1,13 +1,32 @@
 import { NextPage } from 'next';
-import { Typography } from '@mui/material';
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
 
 import { ShopLayout } from '@/components';
+import { initialData } from '@/database';
 
 const HomePage: NextPage = () => {
   return (
     <ShopLayout title={'Shop - Home'} pageDescription={'Find the best products'}>
       <Typography variant='h1' component='h1'>Shop</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>All the products</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map( product => (
+            <Grid item xs={6} sm={4} key={product.slug}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    component='img'
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </ShopLayout>
   )
 }
