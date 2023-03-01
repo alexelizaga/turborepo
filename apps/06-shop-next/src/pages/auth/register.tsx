fbaf5fe03caa5361878ec6ec1c80d7a05bab1f27
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-03-01 18:09:18 +0100

    login and register page

---
+++ apps/06-shop-next/src/pages/auth/register.tsx
@@ -0,0 +1,49 @@
+import NextLink from 'next/link';
+import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material"
+
+import { AuthLayout } from "@/components"
+
+const RegisterPage = () => {
+  return (
+    <AuthLayout title="Register">
+      <Box sx={{ width: 350, p: '10px 20px'}}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h1" component="h1">Create account</Typography>
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField label="Full name" variant="filled" fullWidth />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField label="Email" variant="filled" fullWidth />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField label="Password" type="password" variant="filled" fullWidth />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button color="secondary" className="circular-btn" size="large" fullWidth>
+              Sign up
+            </Button>
+          </Grid>
+
+          <Grid item xs={12} display='flex' justifyContent='end'>
+            <Link
+              href="/auth/login"
+              component={NextLink}
+              color="text.primary"
+              underline='always'
+            >
+              do you have an account?
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </AuthLayout>
+  )
+}
+
+export default RegisterPage;
