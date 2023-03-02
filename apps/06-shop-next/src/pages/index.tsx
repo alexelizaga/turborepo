568be984db297f508f4352f24493ae112d216ae1
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-03-02 07:22:33 +0100

    swr, use products and loading screen

--- apps/06-shop-next/src/pages/index.tsx
+++ apps/06-shop-next/src/pages/index.tsx
@@ -1,17 +1,24 @@
 import { NextPage } from 'next';
 import { Typography } from '@mui/material';
 
-import { ShopLayout, ProductList } from '@/components';
-import { initialData } from '@/database';
-import { IProduct } from '@/interfaces';
+import { ShopLayout, ProductList, FullScreenLoader } from '@/components';
+import { useProducts } from '@/hooks';
 
 const HomePage: NextPage = () => {
+
+  const { products, isLoading, isError} = useProducts('/products');
+
   return (
     <ShopLayout title={'Shop - Home'} pageDescription={'Find the best products'}>
       <Typography variant='h1' component='h1'>Shop</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>All the products</Typography>
 
-      <ProductList products={initialData.products as IProduct[]}  />
+      {
+        isLoading
+          ? <FullScreenLoader />
+          : <ProductList products={products}  />
+      }
+
     </ShopLayout>
   )
 }
