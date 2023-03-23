0879456f9fcb726a546da466953e857555678dff
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-03-23 12:09:14 +0100

    create jwt

--- apps/06-shop-next/src/pages/api/user/login.ts
+++ apps/06-shop-next/src/pages/api/user/login.ts
@@ -3,6 +3,7 @@ import bcryptjs from 'bcryptjs';
 
 import { db } from '@/database';
 import { User } from '@/models';
+import { jwt } from '@/utils';
 
 type Data =
   | { message: string }
@@ -42,10 +43,12 @@ const loginUser = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
     return res.status(400).json({ message: 'Invalid Email or Password - Password' });
   }
 
-  const { role, name } = user;
+  const { role, name, _id } = user;
+
+  const token = jwt.signToken(_id, email);
 
   return res.status(200).json({
-    token: '', //jwt
+    token,
     user: {
       email, role, name
     }
