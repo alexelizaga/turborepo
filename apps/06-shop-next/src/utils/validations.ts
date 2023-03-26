8f2f5297c650c3e33f5cd497ff78b7ebd6189ee9
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-03-26 19:15:07 +0200

    fix regex

--- apps/06-shop-next/src/utils/validations.ts
+++ apps/06-shop-next/src/utils/validations.ts
@@ -1,9 +1,7 @@
 export const isValidEmail = (email: string): boolean => {
   const match = String(email)
     .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+    .match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i);
 
   return !!match;
 };
