ec5ea3887f6902f4866e19d09576f6ccf6e489f2
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-03-29 08:21:57 +0200

    AuthContext

---
+++ apps/06-shop-next/src/context/auth/AuthProvider.tsx
@@ -0,0 +1,29 @@
+import { FC, useReducer, ReactNode } from 'react';
+
+import { AuthContext, authReducer } from '@/context';
+import { IUser } from '@/interfaces';
+
+export interface AuthState {
+  isLoggedIn: boolean;
+  user?: IUser
+}
+
+const AUTH_INITIAL_STATE: AuthState = {
+  isLoggedIn: false,
+  user: undefined,
+}
+
+export const AuthProvider: FC<{children: ReactNode}> = ({ children }) => {
+  const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        ...state,
+        // Methods
+      }}
+    >
+      { children }
+    </AuthContext.Provider>
+  )
+}
