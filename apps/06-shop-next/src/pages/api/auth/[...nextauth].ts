9c1180a005792d6003f2b458468666d7acb648be
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-03-31 13:07:40 +0200

    NextAuth Callbacks

--- apps/06-shop-next/src/pages/api/auth/[...nextauth].ts
+++ apps/06-shop-next/src/pages/api/auth/[...nextauth].ts
@@ -11,7 +11,7 @@ export const authOptions = {
         email: { label: 'Email', type: 'email', placeholder: 'email@google.com' },
         password: { label: 'Password', type: 'password', placeholder: 'Password' }
       },
-      async authorize(credentials) {
+      async authorize(credentials): Promise<any> {
         console.log({ credentials });
         return { name: 'Alex', email: 'alex@google.com', role: 'admin' };
       }
@@ -25,7 +25,35 @@ export const authOptions = {
 
   // Callbacks
   callbacks: {
-    
+    async jwt({ token, account, user }: any) {
+      // console.log({ token, account, user });
+
+      if (account) {
+        token.accessToken = account.access_token;
+
+        switch (account.type) {
+          case 'oauth':
+            // TODO: user verification
+            break;
+          case 'credentials':
+            token.user = user;
+            break;
+        
+          default:
+            break;
+        }
+      }
+
+      return token;
+    },
+    async session({ session, token, user }: any) {
+      // console.log({ session, token, user })
+
+      session.accessToken = token.access_token;
+      session.user = token.user;
+
+      return session;
+    }
   }
 }
 export default NextAuth(authOptions)
