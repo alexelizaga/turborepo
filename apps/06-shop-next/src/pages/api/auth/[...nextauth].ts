50c7f72f2b6681f9496515f24517d92ba4c817ae
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-03-31 12:25:11 +0200

    Provider - credentials

--- apps/06-shop-next/src/pages/api/auth/[...nextauth].ts
+++ apps/06-shop-next/src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,31 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import Credentials from "next-auth/providers/credentials"
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: { label: 'Email', type: 'email', placeholder: 'email@google.com' },
+        password: { label: 'Password', type: 'password', placeholder: 'Password' }
+      },
+      async authorize(credentials) {
+        console.log({ credentials });
+        return { name: 'Alex', email: 'alex@google.com', role: 'admin' };
+      }
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID || '',
       clientSecret: process.env.GITHUB_SECRET || '',
     }),
     // ...add more providers here
   ],
+
+  // Callbacks
+  callbacks: {
+    
+  }
 }
 export default NextAuth(authOptions)
