97fd622eb3386dee005f0d3bb1ece649c51a4e00
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-04-03 16:48:27 +0200

    Paypal oAuth Token

--- apps/06-shop-next/src/pages/api/orders/pay.ts
+++ apps/06-shop-next/src/pages/api/orders/pay.ts
@@ -1,4 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
 type Data = {
   message: string
@@ -15,8 +16,46 @@ export default function handler (req: NextApiRequest, res: NextApiResponse<Data>
   }
 }
 
-const payOrder = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  
+const getPaypalBearerToken = async (): Promise<string|null> => {
 
-  return res.status(200).json({ message: 'Paid order' });
+  const PAYPAL_CLIENT = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;
+  const PAYPAL_SECRET = process.env.PAYPAL_SECRET;
+  
+  const base64Token = Buffer.from(`${ PAYPAL_CLIENT }:${ PAYPAL_SECRET }`, 'utf-8').toString('base64');
+  const body = new URLSearchParams('grant_type=client_credentials');
+
+  try {
+
+    const { data } = await axios.post(process.env.PAYPAL_OAUTH_URL || '', body, {
+      headers: {
+        'Authorization': `Basic ${ base64Token }`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+
+    return data.access_token;
+
+  } catch (error) {
+
+    if (axios.isAxiosError(error)) {
+      console.log(error.response?.data);
+    } else {
+      console.log(error);
+    }
+
+    return null;
+
+  }
+
+}
+
+const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  
+  const paypalBearerToken = await getPaypalBearerToken();
+
+  if (!paypalBearerToken) {
+    return res.status(400).json({ message: "Can't confirm paypal token" });
+  }
+
+  return res.status(200).json({ message: paypalBearerToken });
 }
