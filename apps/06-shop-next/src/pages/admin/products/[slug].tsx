8b16220b01134e704c2977b260d1b417495a969a
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-04-12 17:45:40 +0200

    REST - Upload file

--- apps/06-shop-next/src/pages/admin/products/[slug].tsx
+++ apps/06-shop-next/src/pages/admin/products/[slug].tsx
@@ -83,13 +83,15 @@ const ProductAdminPage: FC<Props> = ({ product }) => {
     setValue("tags", updatedTags, { shouldValidate: true })
   };
 
-  const onFileSelected = ({ target }: ChangeEvent<HTMLInputElement>) => {
+  const onFileSelected = async ({ target }: ChangeEvent<HTMLInputElement>) => {
     if (!target.files || target.files?.length === 0) return;
 
     try {
       for ( const file of target.files) {
         const formData = new FormData();
-        console.log( file );
+        formData.append('file', file);
+        const { data } = await shopApi.post<{ message: string }>('/admin/upload', formData);
+        console.log(data);
       }
     } catch (error) {
       console.log(error);
