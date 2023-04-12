25876def4de91decdb1ee4acede650bec6313910
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-04-12 10:23:45 +0200

    REST - Update product

--- apps/06-shop-next/src/pages/api/admin/products.ts
+++ apps/06-shop-next/src/pages/api/admin/products.ts
@@ -3,16 +3,19 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { IProduct } from '@/interfaces';
 import { db } from '@/database';
 import { Product } from '@/models';
+import { isValidObjectId } from 'mongoose';
 
 type Data =
   | { message: string }
   | IProduct[]
+  | IProduct
 
 export default function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
   switch (req.method) {
     case 'GET':
       return getProducts(req, res);
     case 'PUT':
+      return updateProduct(req, res);
     case 'POST':
   
     default:
@@ -31,3 +34,38 @@ const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
   return res.status(200).json(products);
 }
+
+const updateProduct = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { _id = '', images = []} = req.body as IProduct;
+
+  if ( !isValidObjectId(_id) ) {
+    return res.status(400).json({ message: "The product id is invalid"});
+  }
+
+  if ( images.length <= 2 ) {
+    return res.status(400).json({ message: "You need at least two images"});
+  }
+
+  try {
+    await db.connect();
+
+    const product = await Product.findById(_id);
+    if (!product) {
+      await db.disconnect();
+      return res.status(400).json({ message: "There is no product with that id"});
+    }
+
+    // TODO: delete Cloudinary images
+
+    await Product.updateOne(req.body);
+    await db.disconnect();
+
+    return res.status(200).json( product );
+  } catch (error) {
+    console.log(error);
+    await db.disconnect();
+    return res.status(400).json({ message: "Review server logs"});
+  }
+
+}
+
