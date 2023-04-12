72a7e683035435d3c8e9a261998b43507309f66c
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-04-12 11:36:25 +0200

    Dev flag in admin navbar

--- apps/06-shop-next/src/components/admin/AdminNavbar.tsx
+++ apps/06-shop-next/src/components/admin/AdminNavbar.tsx
@@ -1,7 +1,7 @@
 
 import { useContext } from 'react';
 import NextLink from 'next/link';
-import { AppBar, Box, Button, Link, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, Chip, Link, Toolbar, Typography } from '@mui/material';
 
 import { UIContext } from '@/context';
 
@@ -19,6 +19,11 @@ export const AdminNavbar = () => {
           display="flex"
           alignItems="center"
         >
+          {
+            process.env.NODE_ENV === 'development' && (
+              <Chip sx={{ mr: 1 }} label="DEV" size="small" color="primary" variant="outlined" />
+            )
+          } 
           <Typography variant="h6">BroCode |</Typography>
           <Typography sx={{ ml: 0.5 }}>Shop</Typography>
         </Link>
