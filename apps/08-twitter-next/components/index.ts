85ba8e59554bf440a2e030d46ff65d6ab93d4e6f
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-04-23 11:07:25 +0200

    Modal component

--- apps/08-twitter-next/components/index.ts
+++ apps/08-twitter-next/components/index.ts
@@ -1,4 +1,6 @@
 export * from './layout/index';
 
+export { default as Button } from './Button';
 export { default as Header } from './Header';
 export { default as Layout } from './Layout';
+export { default as Modal } from './Modal';
