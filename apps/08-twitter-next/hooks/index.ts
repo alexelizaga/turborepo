76139165bb156ff876787560484d855cb5661582
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-04-29 09:23:34 +0200

    postview

--- apps/08-twitter-next/hooks/index.ts
+++ apps/08-twitter-next/hooks/index.ts
@@ -1,6 +1,7 @@
 export { default as useCurrentUser } from './useCurrentUser';
 export { default as useEditModal } from './useEditModal';
 export { default as useLoginModal } from './useLoginModal';
+export { default as usePost } from './usePost';
 export { default as usePosts } from './usePosts';
 export { default as useRegisterModal } from './useRegisterModal';
 export { default as useUser } from './useUser';
