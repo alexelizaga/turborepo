3449223958294baceb2f346109cc385dbeadf2a9
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-05-01 09:49:54 +0200

    setup the env

---
+++ apps/09-airbnb-next/app/layout.tsx
@@ -0,0 +1,26 @@
+import { Nunito } from 'next/font/google';
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb clone',
+}
+
+const font = Nunito({
+  subsets: ["latin"]
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
