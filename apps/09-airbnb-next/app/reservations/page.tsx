1d1168fdf0b34f64e01676b4e66e4acc02b1dde6
alexelizaga <83604703+alexelizaga@users.noreply.github.com>
2023-05-23 07:24:47 +0200

    Reservations screen

---
+++ apps/09-airbnb-next/app/reservations/page.tsx
@@ -0,0 +1,38 @@
+import { EmptyState } from "@/components";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+
+import ReservationsClient from "./ReservationsClient";
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if(!currentUser) (
+    <EmptyState
+      title="Unauthorized"
+      subtitle="Please login"
+    />
+  )
+
+  const reservations = await getReservations({
+    authorId: currentUser?.id
+  });
+
+  if(reservations.length === 0) (
+    <EmptyState
+      title="Not reservations found"
+      subtitle="Looks like you have no reservations on your properties"
+    />
+  )
+
+  return (
+    <ReservationsClient
+      reservations={reservations}
+      currentUser={currentUser}
+
+    />
+  )
+}
+
+export default ReservationsPage;
