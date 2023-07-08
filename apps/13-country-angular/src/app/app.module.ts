62b7e8e009d44009fb2bbd148158836465d289b9
alexelizaga <aelizag@corp.vodafone.es>
2023-07-08 18:21:21 +0200

    Section 9

--- apps/13-country-angular/src/app/app.module.ts
+++ apps/13-country-angular/src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+
 
 
 @NgModule({
@@ -11,8 +14,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    SharedModule
+    HttpClientModule,
+
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
