db1cad980b4ce54bb9a52d9aba1b6f48d9f180df
alexelizaga <alexelizaga@icloud.com>
2023-07-24 11:56:28 +0200

    158

--- apps/14-pipes-angular/src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ apps/14-pipes-angular/src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.sass']
+  styleUrls: ['./uncommon-page.component.sass'],
 })
 export class UncommonPageComponent {
+  // i18nSelect
+  public name: string = 'Alex';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Lidia';
+    this.gender = 'female';
+  }
 }
