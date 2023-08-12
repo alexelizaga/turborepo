17bd253b7abd05ac070ab2699af2a1b40972ee93
alexelizaga <alexelizaga@icloud.com>
2023-08-12 13:38:55 +0200

    207

--- apps/15-heroes-angular/src/app/heroes/pages/new-page/new-page.component.ts
+++ apps/15-heroes-angular/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { Publisher } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -6,6 +9,18 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class NewPageComponent {
+  public id = new FormControl('');
+
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
   public publishers = [
     { id: 'DC Comics', desc: 'DC - Comics' },
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
