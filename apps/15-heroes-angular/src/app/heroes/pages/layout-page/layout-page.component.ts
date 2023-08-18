5610eec26ea34411d92b527df74365d01eb548d8
alexelizaga <alexelizaga@icloud.com>
2023-08-18 17:29:29 +0200

    225

--- apps/15-heroes-angular/src/app/heroes/pages/layout-page/layout-page.component.ts
+++ apps/15-heroes-angular/src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../auth/services/auth.service';
+import { User } from '../../../auth/interfaces/user.interface';
 
 @Component({
   selector: 'app-layout-page',
@@ -11,4 +15,15 @@ export class LayoutPageComponent {
     { label: 'Add', icon: 'add', url: './new-hero' },
     { label: 'Search', icon: 'search', url: './search' },
   ];
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  public get user(): User | undefined {
+    return this.authService.currentUser;
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
 }
