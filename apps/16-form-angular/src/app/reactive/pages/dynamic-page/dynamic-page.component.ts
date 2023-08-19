5415d48d6ccf332484971fce1fedf5e89fabfdb0
alexelizaga <alexelizaga@icloud.com>
2023-08-19 17:42:31 +0200

    240

--- apps/16-form-angular/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ apps/16-form-angular/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -19,6 +19,39 @@ export class DynamicPageComponent {
 
   constructor(private fb: FormBuilder) {}
 
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  isValidFieldInArray(formArray: FormArray, index: number): boolean | null {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres.`;
+      }
+    }
+
+    return null;
+  }
+
+  onDeleteFavorite(index: number): void {
+    this.favoriteGames.removeAt(index);
+  }
+
   onSubmit(): void {
     if (this.myForm.invalid) {
       this.myForm.markAllAsTouched();
