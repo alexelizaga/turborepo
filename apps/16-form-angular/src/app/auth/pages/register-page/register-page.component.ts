6d9b922660d5da821a2d9e0feac6ed493ccd1158
alexelizaga <alexelizaga@icloud.com>
2023-08-20 09:13:43 +0200

    248

--- apps/16-form-angular/src/app/auth/pages/register-page/register-page.component.ts
+++ apps/16-form-angular/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styleUrls: [],
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string): boolean | null {
+    return (
+      this.myForm.controls[field].errors && this.myForm.controls[field].touched
+    );
+  }
+
+  onSubmit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+  }
+}
