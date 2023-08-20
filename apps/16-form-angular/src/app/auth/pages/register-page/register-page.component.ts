27c284260e72072a85a841d665a1fd92b407f413
alexelizaga <alexelizaga@icloud.com>
2023-08-20 11:36:09 +0200

    250

--- apps/16-form-angular/src/app/auth/pages/register-page/register-page.component.ts
+++ apps/16-form-angular/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  MyValidators,
+  emailPattern,
+  firstNameAndLastnamePattern,
+} from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
 })
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required]],
-    username: ['', [Validators.required]],
+    name: ['', [Validators.required, MyValidators.firstNameAndLastname]],
+    email: ['', [Validators.required, MyValidators.email]],
+    username: ['', [Validators.required, MyValidators.cantBeStrider]],
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required]],
   });
---
+++ apps/16-form-angular/src/app/shared/validators/validators.ts
@@ -0,0 +1,46 @@
+import { FormControl, ValidationErrors } from '@angular/forms';
+
+export const firstNameAndLastnamePattern: RegExp = /^([a-zA-Z]+) ([a-zA-Z]+)$/;
+export const emailPattern: RegExp =
+  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+const pattern = (
+  control: FormControl,
+  pattern: RegExp
+): ValidationErrors | null => {
+  const value: string = control.value.trim();
+
+  if (!pattern.test(value)) {
+    return {
+      requiredPattern: `${pattern}`,
+    };
+  }
+
+  return null;
+};
+
+export class MyValidators {
+  constructor() {}
+
+  static cantBeStrider = (control: FormControl): ValidationErrors | null => {
+    const value = control.value.trim().toLowerCase();
+
+    if (value === 'strider') {
+      return {
+        noStrider: true,
+      };
+    }
+
+    return null;
+  };
+
+  static email = (control: FormControl): ValidationErrors | null => {
+    return pattern(control, emailPattern);
+  };
+
+  static firstNameAndLastname = (
+    control: FormControl
+  ): ValidationErrors | null => {
+    return pattern(control, firstNameAndLastnamePattern);
+  };
+}
