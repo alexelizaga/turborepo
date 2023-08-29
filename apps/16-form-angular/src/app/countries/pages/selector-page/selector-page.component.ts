d7dfe8d5159cc379413d27137673235acec4521c
alexelizaga <alexelizaga@icloud.com>
2023-08-29 08:47:52 +0200

    267

--- apps/16-form-angular/src/app/countries/pages/selector-page/selector-page.component.ts
+++ apps/16-form-angular/src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
@@ -35,6 +35,7 @@ export class SelectorPageComponent implements OnInit {
     this.myForm
       .get('region')!
       .valueChanges.pipe(
+        tap(() => this.myForm.get('country')!.setValue('')),
         switchMap((region) =>
           this.countriesService.getCountriesByRegion(region)
         )
