57f3344a1a21556cd696dbca8124bbea87d07390
alexelizaga <alexelizaga@icloud.com>
2023-08-29 15:53:08 +0200

    283

---
+++ apps/17-maps-angular/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maps-markers-page',
+  templateUrl: './markers-page.component.html',
+  styles: [],
+})
+export class MarkersPageComponent {}
---
+++ apps/17-maps-angular/src/app/maps/pages/properties-page/properties-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'maps-properties-page',
+  templateUrl: './properties-page.component.html',
+  styles: [],
+})
+export class PropertiesPageComponent {}
