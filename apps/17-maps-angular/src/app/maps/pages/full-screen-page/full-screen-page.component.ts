a5ae65d4582e6f5d85f2c1204f6215f073bbc305
alexelizaga <alexelizaga@icloud.com>
2023-08-29 17:12:06 +0200

    288

--- apps/17-maps-angular/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ apps/17-maps-angular/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl';
 
 @Component({
   selector: 'maps-full-screen-page',
   templateUrl: './full-screen-page.component.html',
-  styles: [],
+  styleUrls: ['./full-screen-page.component.css'],
 })
-export class FullScreenPageComponent {}
+export class FullScreenPageComponent implements AfterViewInit {
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  ngAfterViewInit(): void {
+    if (!this.divMap) throw 'HTML element not found';
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+}
