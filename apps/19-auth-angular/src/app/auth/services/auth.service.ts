2858c56e2350188cf06fdfec8cb6d51f1194c3de
alexelizaga <alexelizaga@icloud.com>
2023-09-10 10:04:18 +0200

    360

--- apps/19-auth-angular/src/app/auth/services/auth.service.ts
+++ apps/19-auth-angular/src/app/auth/services/auth.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+import { environments } from '../../../environments/environments';
+import { AuthStatus, User } from '../interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly baseUrl: string = environments.baseUrl;
+  private http = inject(HttpClient);
 
-  constructor() { }
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
+  constructor() {}
+
+  login(email: string, password: string): Observable<boolean> {
+    return of(true);
+  }
 }
