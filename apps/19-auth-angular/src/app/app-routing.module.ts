8f8dca45ed35de60b22f728db80a404e2295b226
alexelizaga <alexelizaga@icloud.com>
2023-09-10 20:43:46 +0200

    365

--- apps/19-auth-angular/src/app/app-routing.module.ts
+++ apps/19-auth-angular/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { isAthenticatedGuard } from './auth/guards/is-athenticated.guard';
+
 const routes: Routes = [
   {
     path: 'auth',
@@ -9,7 +11,7 @@ const routes: Routes = [
   },
   {
     path: 'dashboard',
-    // guards
+    canActivate: [isAthenticatedGuard],
     loadChildren: () =>
       import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
   },
--- apps/19-auth-angular/src/app/auth/pages/login-page/login-page.component.ts
+++ apps/19-auth-angular/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 import { AuthService } from '../../services/auth.service';
 
@@ -11,17 +13,21 @@ import { AuthService } from '../../services/auth.service';
 export class LoginPageComponent {
   private fb = inject(FormBuilder);
   private authService = inject(AuthService);
+  private router = inject(Router);
 
   public myForm: FormGroup = this.fb.group({
-    email: ['', [Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    email: ['alex@google.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
   });
 
   login(): void {
     const { email, password } = this.myForm.value;
 
-    this.authService.login(email, password).subscribe((success) => {
-      console.log({ success });
+    this.authService.login(email, password).subscribe({
+      next: () => this.router.navigateByUrl('/dashboard'),
+      error: (message) => {
+        Swal.fire('Error', message, 'error');
+      },
     });
   }
 }
